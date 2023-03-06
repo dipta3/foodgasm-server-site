cdd039dd68a9fadac2a17e206a887ea8388f1208
Dipta Biswas <diptabiswas79@gmail.com>
2023-03-06 17:31:53 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { error } = require('console');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,10 +10,32 @@ app.use(express.json())
 app.use(cors())
 
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hc7ua.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const foodGasmCollention = client.db('foodGasm').collection('foods');
+
+        app.get('/foods', async (req, res) => {
+            const query = {}
+            const cursor = foodGasmCollention.find(query)
+            const foods = await cursor.toArray();
+            res.send(foods)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.log(error))
+
+
 app.get('/', (req, res) => {
-    res.send('Foodgasm server Running')
+    res.send('Food Gasm Server Running')
 })
 
 app.listen(port, () => {
-    console.log(`Foodgasm running on ${port}`)
+    console.log(`Food Gasm Running On ${port}`)
 })
